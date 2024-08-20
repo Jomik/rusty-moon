d692b52ca4169523a3e81419a341d59b7bfb7d0a
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2024-08-20 14:10:10 +0200

    feat(discord): set presence

--- src/discord.rs
+++ src/discord.rs
@@ -1,19 +1,28 @@
 use std::{
     future::{Future, IntoFuture},
     pin::Pin,
-    sync::Arc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
 };
 
 use anyhow::Result;
-use serenity::all::{
-    CreateAttachment, CreateEmbed, CreateMessage, EditMessage, GetMessages, Http, UserId,
+use serenity::{
+    all::{
+        ActivityData, Context, CreateAttachment, CreateEmbed, CreateMessage, EditMessage,
+        EventHandler, GatewayIntents, GetMessages, OnlineStatus, Ready, UserId,
+    },
+    async_trait,
+    prelude::TypeMapKey,
+    Client,
 };
 use tokio::{
     select,
-    sync::{mpsc, watch},
+    sync::{mpsc, watch, Mutex},
 };
 
-use crate::moonraker;
+use crate::moonraker::{self, State};
 
 #[derive(Debug, Clone, serde::Deserialize)]
 pub struct Config {
@@ -36,11 +45,14 @@ impl IntoFuture for ServiceBuilder {
     type IntoFuture = Pin<Box<dyn Future<Output = Self::Output>>>;
 
     fn into_future(self) -> Self::IntoFuture {
-        let user = UserId::new(self.config.user_id);
         Box::pin(async move {
-            // let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-            // let client = Client::builder(self.config.token, intents).await?;
-            let client = Arc::new(Http::new(&self.config.token));
+            let user = UserId::new(self.config.user_id);
+
+            let client = Client::builder(self.config.token, GatewayIntents::default())
+                .event_handler(Handler {
+                    is_loop_running: AtomicBool::new(false),
+                })
+                .await?;
             Ok(Service {
                 client,
                 user_id: user,
@@ -49,8 +61,42 @@ impl IntoFuture for ServiceBuilder {
     }
 }
 
+struct StatusChannel;
+impl TypeMapKey for StatusChannel {
+    type Value = Arc<Mutex<watch::Receiver<moonraker::Status>>>;
+}
+
+struct NotificationChannel;
+impl TypeMapKey for NotificationChannel {
+    type Value = Arc<Mutex<mpsc::Receiver<moonraker::Notification>>>;
+}
+
+struct OwnerId;
+impl TypeMapKey for OwnerId {
+    type Value = Arc<UserId>;
+}
+
+struct Handler {
+    is_loop_running: AtomicBool,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, _data: Ready) {
+        let ctx = Arc::new(ctx);
+        if self.is_loop_running.load(Ordering::Relaxed) {
+            return;
+        }
+
+        let ctx = Arc::clone(&ctx);
+        tokio::spawn(async move { run(&ctx).await });
+
+        self.is_loop_running.swap(true, Ordering::Relaxed);
+    }
+}
+
 pub struct Service {
-    client: Arc<Http>,
+    client: Client,
     user_id: UserId,
 }
 
@@ -60,69 +106,114 @@ impl Service {
     }
 
     pub async fn start(
-        self,
-        mut status_rx: watch::Receiver<moonraker::Status>,
-        mut notification_rx: mpsc::Receiver<moonraker::Notification>,
+        mut self,
+        status_rx: watch::Receiver<moonraker::Status>,
+        notification_rx: mpsc::Receiver<moonraker::Notification>,
     ) -> Result<()> {
-        let user = self.client.get_user(self.user_id).await?;
-        let bot = self.client.get_current_user().await?;
-        let channel = user.create_dm_channel(&self.client).await?;
-        let message_ids = channel
-            .messages(&self.client, GetMessages::new())
-            .await?
-            .into_iter()
-            .filter(|msg| msg.author.id == bot.id);
+        {
+            let mut data = self.client.data.write().await;
 
-        for message in message_ids {
-            message.delete(&self.client).await?;
+            data.insert::<OwnerId>(Arc::new(self.user_id));
+            data.insert::<StatusChannel>(Arc::new(Mutex::new(status_rx)));
+            data.insert::<NotificationChannel>(Arc::new(Mutex::new(notification_rx)));
         }
+        self.client.start().await?;
+        Ok(())
+    }
+}
 
-        let status = status_rx.borrow_and_update().clone();
-        let message_builder = CreateMessage::new().embeds(self.get_status_embeds(status));
-        let mut message = user.direct_message(&self.client, message_builder).await?;
-        message.pin(&self.client).await?;
+async fn run(ctx: &Context) -> Result<()> {
+    let user_id = {
+        let data_read = ctx.data.read().await;
+        data_read.get::<OwnerId>().unwrap().as_ref().to_owned()
+    };
+    let user = ctx.http.get_user(user_id).await?;
+    let bot = ctx.http.get_current_user().await?;
 
-        loop {
-            // TODO: handle errors
-            select! {
-                Ok(()) = status_rx.changed() =>{
-                    let edit_builder = EditMessage::new().embeds(self.get_status_embeds(status_rx.borrow_and_update().clone()));
-                    message.edit(&self.client, edit_builder).await?;
-                },
-                Some(notification) = notification_rx.recv() => {
-                    let mut message_builder = CreateMessage::new().content(notification.message);
-                    if let Some(image) =  notification.image {
-                        message_builder = message_builder.add_file(CreateAttachment::file(&image.into(), "image.png").await?);
-                    };
-                    user.direct_message(&self.client, message_builder).await?;
-                },
-            }
-        }
+    let channel = user.create_dm_channel(&ctx.http).await?;
+    let message_ids = channel
+        .messages(&ctx.http, GetMessages::new())
+        .await?
+        .into_iter()
+        .filter(|msg| msg.author.id == bot.id);
+
+    for message in message_ids {
+        message.delete(&ctx.http).await?;
     }
 
-    fn get_status_embeds(&self, status: moonraker::Status) -> Vec<CreateEmbed> {
-        let printer_status = CreateEmbed::new().title("Printer status").field(
-            "State",
-            format!("{:?}", status.state),
-            false,
-        );
+    let status_rx = {
+        let data_read = ctx.data.read().await;
+        data_read.get::<StatusChannel>().unwrap().clone()
+    };
 
-        let job_status = status.printer.map(|printer| {
-            printer.job.map_or_else(
-                || CreateEmbed::new().title("Job status").description("No job"),
-                |job| {
-                    CreateEmbed::new().title("Job status").field(
-                        "Layer",
-                        format!("{} / {}", job.current_layer, job.total_layer),
-                        true,
-                    )
-                },
-            )
-        });
+    let notification_rx = {
+        let data_read = ctx.data.read().await;
+        data_read.get::<NotificationChannel>().unwrap().clone()
+    };
 
-        vec![Some(printer_status), job_status]
-            .into_iter()
-            .flatten()
-            .collect()
+    let status = status_rx.lock().await.borrow_and_update().clone();
+    let message_builder = CreateMessage::new().embeds(get_status_embeds(status));
+    let mut message = user.direct_message(&ctx.http, message_builder).await?;
+    message.pin(&ctx.http).await?;
+
+    loop {
+        let mut status_rx_lock = status_rx.lock().await;
+        let mut notification_rx_lock = notification_rx.lock().await;
+        // TODO: handle errors
+        select! {
+            Ok(()) = status_rx_lock.changed() => {
+                let status = status_rx_lock.borrow_and_update().clone();
+                match status.state {
+                    State::Disconnected | State::Shutdown(_) | State::Error(_) => {
+                        ctx.set_presence(Some(ActivityData::custom("Disconnected")), OnlineStatus::Offline);
+                    },
+                    State::Printing => {
+                        ctx.set_presence(Some(ActivityData::custom("Printing")), OnlineStatus::DoNotDisturb);
+                    },
+                    State::Paused => {
+                        ctx.set_presence(Some(ActivityData::custom("Paused")), OnlineStatus::Idle);
+                    },
+                    State::Startup | State::Standby | State::Complete => {
+                        ctx.set_presence(Some(ActivityData::custom("Standby")), OnlineStatus::Online);
+                    },
+                }
+
+                let edit_builder = EditMessage::new().embeds(get_status_embeds(status));
+                message.edit(&ctx.http, edit_builder).await?;
+            },
+            Some(notification) = notification_rx_lock.recv() => {
+                let mut message_builder = CreateMessage::new().content(notification.message);
+                if let Some(image) =  notification.image {
+                    message_builder = message_builder.add_file(CreateAttachment::file(&image.into(), "image.png").await?);
+                };
+                user.direct_message(&ctx.http, message_builder).await?;
+            },
+        }
     }
+}
+
+fn get_status_embeds(status: moonraker::Status) -> Vec<CreateEmbed> {
+    let printer_status = CreateEmbed::new().title("Printer status").field(
+        "State",
+        format!("{:?}", status.state),
+        false,
+    );
+
+    let job_status = status.printer.map(|printer| {
+        printer.job.map_or_else(
+            || CreateEmbed::new().title("Job status").description("No job"),
+            |job| {
+                CreateEmbed::new().title("Job status").field(
+                    "Layer",
+                    format!("{} / {}", job.current_layer, job.total_layer),
+                    true,
+                )
+            },
+        )
+    });
+
+    vec![Some(printer_status), job_status]
+        .into_iter()
+        .flatten()
+        .collect()
 }
